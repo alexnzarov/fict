e8693329a38380188b75ba5ac16d41b61b34fb1c
Alexander <alxkrekeris@gmail.com>
2019-07-31 23:41:32 +0300

    split bot commands

---
+++ server/src/bot/help.ts
@@ -0,0 +1,10 @@
+import { ContextMessageUpdate } from 'telegraf';
+import bot from './';
+
+const message = [
+	'Я ничего не умею, а ты?',
+].join('\n');
+
+bot.command('/help', (ctx: ContextMessageUpdate) => {
+		ctx.replyWithHTML(message);
+});
--- server/src/bot/index.ts
+++ server/src/bot/index.ts
@@ -1,31 +1,10 @@
 import Telegraf, { ContextMessageUpdate } from 'telegraf';
 import AppConfig from '../config';
 
-import sendAuthMessage from './auth';
-
 const bot = new Telegraf<ContextMessageUpdate>(AppConfig.BOT_TOKEN);
+export default bot;
 
-const helloMessage = [
-	'<b>Привет, я дружелюбный FICT Robot.</b>\n',
-	'Моя цель существования - помощь студентам.',
-	'Чтобы узнать больше о том, что я умею, напиши /help.\n',
-	'Так же можешь ознакомиться с нашими ресурсами, которые я указал ниже.\n',
-	`Наш сайт: https://${AppConfig.HOST}/`,
-	'Официальный чат: https://t.me/fict_talk',
-	'Официальный канал: https://t.me/fict_time',
-].join('\n');
-
-bot.start((ctx) => {
-	const tokens = ctx.message.text.split(' ');
-	if (tokens[1] === 'auth') {
-		return sendAuthMessage(ctx);
-	}
-
-	ctx.replyWithHTML(helloMessage);
-});
-
-bot.command('/help', (ctx) => {
-	ctx.reply('Я пока ничего не умею, а ты?');
-});
+import './start';
+import './help';
 
 bot.launch();
