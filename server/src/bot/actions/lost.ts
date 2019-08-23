29dab09dae31782542df925b5fb61ac7694b8cc5
Alexander <alxkrekeris@gmail.com>
2019-08-23 21:28:52 +0300

    continued working on "lost" feature

--- server/src/bot/actions/lost.ts
+++ server/src/bot/actions/lost.ts
@@ -2,21 +2,24 @@ import AppConfig from '../../config';
 import InlineKeyboard from '../utils/InlineKeyboard';
 import bot from '..';
 
-bot.command('/lost', (ctx) => {
+bot.command('/lost', (ctx) => ctx.replyWithHTML('Ты потерялся? Ничего, не бойся.\n<b>Отправь мне свою локацию через вложения или нажми на кнопку "Я потерялся", чтобы я смог тебе помочь!</b>'))
 
-});
+const instructions = [
+  '<b>Твоя локация была передана в надёжные руки!</b>\n',
+  'Оставайся там, где ты оставил(а) метку.',
+  'Скоро с тобой свяжутся и помогут с твоей проблемой.\n',
+  '<i>Если твоя проблема решилась, пожалуйста, нажми на кнопку "Со мной всё хорошо", чтобы мы не волновались.</i>',
+].join('\n');
 
 bot.on('location', (ctx) => {
   const kb = InlineKeyboard(
-    [ { 
-      text: 'Мне помогли',
-      callback_data: `l_helped_${ctx.from.id}`
-    } ], 
-    [ { 
-      text: 'Отменить',
-      callback_data: `l_canceled_${ctx.from.id}`
-    } ], 
+    [ 
+      { 
+        text: 'Со мной всё хорошо',
+        callback_data: `l_helped_${ctx.from.id}`,
+      },
+    ]
   );
-  ctx.reply(`<b>Я передал твою локацию, теперь стой на месте!</b>\n\nВ скором времени к тебе подойдут, чтобы помочь, или свяжутся через личные сообщения.`, kb);
-  ctx.replyWithLocation(ctx.message.location.latitude, ctx.message.location.longitude);
+  
+  ctx.replyWithHTML(instructions, kb);
 });
