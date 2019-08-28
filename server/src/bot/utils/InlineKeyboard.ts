35910efdb0a78cc82a0174d9a91e6fc44a54ad5d
Alexander <alxkrekeris@gmail.com>
2019-08-28 09:45:32 +0300

    added phone number to lost feature

--- server/src/bot/utils/InlineKeyboard.ts
+++ server/src/bot/utils/InlineKeyboard.ts
@@ -4,6 +4,7 @@ export default (...buttons: InlineKeyboardButton[][]): ExtraReplyMessage => {
   return {
     reply_markup: {
       inline_keyboard: buttons,
+      resize_keyboard: true,
     },
   };
 };
